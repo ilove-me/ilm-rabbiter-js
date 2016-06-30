8347228ec56c4a3c53511c511a1988ce151b93ec
João Teixeira <jldteixeira@gmail.com>
2016-06-30 17:05:38 +0100

    remove debug logger calls

--- src/callbacks.js
+++ src/callbacks.js
@@ -7,26 +7,22 @@ class Callbacks {
   }
 
   addSent(key, callback) {
-    logger.info('Callbacks addSent', key);
     if(key){
       this.sentCallbacksMap[key] = callback;
     }
   }
 
   removeSent(key) {
-    logger.info('Callbacks removeSent', key);
     var callbackFunction = this.sentCallbacksMap[key];
     delete this.sentCallbacksMap[key];
     return callbackFunction;
   }
 
   addMessage(key, callback) {
-    logger.info('Callbacks addMessage', key);
     this.receivedCallbacksMap[key] = callback;
   }
 
   removeMessage(key) {
-    logger.info('Callbacks removeMessage', key);
     return this.receivedCallbacksMap[key];
   }
 }
